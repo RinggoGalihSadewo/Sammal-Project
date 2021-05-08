661e3303597710d57f77592ff9894c8020440f43
RinggoGalihSadewo <ringgo.118140076@student.itera.ac.id>
2021-05-08 16:14:25 +0700

    membuat index.js

---
+++ Sammal Project/src/pages/index.js
@@ -0,0 +1,31 @@
+import AdminDataPengguna from './AdminDataPengguna';
+import AdminHome from './AdminHome';
+import AdminMateri from './AdminMateri';
+import AdminPengguna from './AdminPengguna';
+import AdminPesananBaruMasuk from './AdminPesananBaruMasuk';
+import AdminPesanKonfirmasi from './AdminPesanKonfirmasi';
+import BuangSampah from './BuangSampah';
+import EmailTerdaftar from './EmailTerdaftar';
+import EmailVerifi from './EmailVerifi';
+import HomeBeranda from './HomeBeranda';
+import HomeGuide from './HomeGuide';
+import HomeMateri from './HomeMateri';
+import HomeTukar from './HomeTukar';
+import LandingPage1 from './LandingPage1';
+import LandingPage2 from './LandingPage2';
+import Login from './Login';
+import Pendaftaran from './Pendaftaran';
+import Profile from './Profile';
+import Sidebar from './Sidebar';
+import SplashScreen from './SplashScreen';
+import UbahProfile from './UbahProfile';
+
+
+export {
+    AdminDataPengguna, AdminHome, AdminMateri, 
+    AdminPengguna, AdminPesananBaruMasuk, AdminPesanKonfirmasi,
+    BuangSampah, EmailTerdaftar, EmailVerifi, HomeBeranda, 
+    HomeGuide, HomeMateri, HomeTukar, LandingPage1, LandingPage2,
+    Login, Pendaftaran, Profile, Sidebar, SplashScreen,
+    UbahProfile
+};
